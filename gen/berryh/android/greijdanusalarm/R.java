2fa2f604b50f3420b7dd182279f0996ce61d22e5
berryh <berryh@aol.nl>
2014-01-09 10:02:31 +0100

    Adding Activities and Menus

--- gen/berryh/android/greijdanusalarm/R.java
+++ gen/berryh/android/greijdanusalarm/R.java
@@ -24,17 +24,22 @@ public final class R {
         public static final int greijdanus=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int menu_item_hoofdscherm_instellingen=0x7f080000;
+        public static final int menu_item_instellingen_hoofdscherm=0x7f080001;
     }
     public static final class layout {
         public static final int activity_hoofdscherm=0x7f030000;
+        public static final int activity_instellingen=0x7f030001;
     }
     public static final class menu {
         public static final int hoofd_scherm=0x7f070000;
+        public static final int instellingen=0x7f070001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int instellingen=0x7f050003;
+        public static final int menu_option_hoofdscherm=0x7f050004;
+        public static final int menu_option_instellingen=0x7f050001;
         public static final int welcome=0x7f050002;
     }
     public static final class style {
--- src/berryh/android/greijdanusalarm/HoofdScherm.java
+++ src/berryh/android/greijdanusalarm/HoofdScherm.java
@@ -2,7 +2,9 @@ package berryh.android.greijdanusalarm;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class HoofdScherm extends Activity {
 
@@ -18,6 +20,20 @@ public class HoofdScherm extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.hoofd_scherm, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	
+    	switch(item.getItemId()){
+    	case R.id.menu_item_hoofdscherm_instellingen:
+    		startActivity(new Intent(this, InstellingenScherm.class));
+    	case R.id.menu_item_instellingen_hoofdscherm:
+    		startActivity(new Intent(this, this.getClass()));
+    	}
+    	
+    	return false;
+    	
     }
     
 }
