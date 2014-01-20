730beecca3a5175436c33d439c12491239d3bf18
berryh <berryh@teamdestroy.tk>
2014-01-20 23:15:37 +0100

    Starting InstellingenScherm Design

--- src/berryh/android/greijdanusalarm/InstellingenScherm.java
+++ src/berryh/android/greijdanusalarm/InstellingenScherm.java
@@ -5,13 +5,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class InstellingenScherm extends Activity{
-	
-	@Override
+
+    private Button btnSave;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instellingen);
+        btnSave = (Button) findViewById(R.id.btnSave);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Instellingen opslaan...", 5).show();
+            }
+        });
+
     }
 
 
